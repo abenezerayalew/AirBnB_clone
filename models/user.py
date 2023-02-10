733c6002f6d4f14ee84ff4ca89b81bb704ca186c
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2023-02-10 21:56:06 +0300

    user

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" User class, inherits from BaseModel """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ User class, inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ User initializer """
+        super().__init__(*args, **kwargs)
