94a11c5f2ee46d741c4236bf5b15d0b1aaa63c43
Obsera-b <111008788+Obsera-b@users.noreply.github.com>
2023-02-12 21:42:41 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -2,8 +2,9 @@
 """ Review class, inherits from BaseModel """
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-    """ Review class, inherits from BaseModel """
+    """ Review class, Reviews made by users about a place """
     place_id = ""
     user_id = ""
     text = ""
