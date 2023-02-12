f51274ec3164b42787274b38cdc9d45296bf9465
Obsera-b <111008788+Obsera-b@users.noreply.github.com>
2023-02-12 21:52:36 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Unittest for User class """
+""" 
+Unittest for User class 
+"""
 import unittest
 from models.user import User
 import datetime
-
 
 class UserCase(unittest.TestCase):
     """ Unittest for User class """
